26817587148a6572045fc4f3562f1aadb4ce0c4e
Vitatiy <targetdeveloper57@gmail.com>
2020-08-22 20:07:38 +0300

    added method updata for projects

--- controller/projectController.js
+++ controller/projectController.js
@@ -23,24 +23,49 @@ class ProjectController {
     create(req, res) {
         let data = req.body;
 
-        const project = new Project({
+        const project = new ProjectModel({
             title: data.title,
             description: data.description
         });
 
         project.save((err, project) => {
             if (err) {
-                return res.send(err);
+                return res.status(400).send(err);
             }
             res.status(200).send('Send Data');
         });
 
     }
 
+    update(req, res) {
+        let data = req.body;
+
+        ProjectModel.update(
+            { _id: req.params.projectID },
+            {
+                $set: {
+                    title: data.title,
+                    description: data.description,
+                }
+            },
+            function (err, result) {
+                if (err) { res.status(400).send(err); return; }
+                if (result) {
+                    res.status(200).send({ resultCode: 0 });
+                } else {
+                    res.send({ resultCode: 1 });
+                }
+            }
+        )
+    }
+
+
+
     delete(req, res) {
         ProjectModel.deleteOne({ _id: req.params.projectID }, function (err) {
-            if (err) return handleError(err);
-            res.status(200).send('remove Data');
+            if (err) return handleError(err)
+
+            res.status(200).send('remove Data')
         });
     }
 }
--- routers/project.routers.js
+++ routers/project.routers.js
@@ -5,7 +5,8 @@ const project = new ProjectController();
 
 router.get('/', project.getAll);
 router.get('/:projectID', project.getOne);
-router.delete('/:projectID', project.delete);
 router.post('/', project.create);
+router.put('/:projectID', project.update);
+router.delete('/:projectID', project.delete);
 
 module.exports = router;
